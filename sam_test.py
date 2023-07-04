98791402f363343c9d257a6f23edc848be7e843d
Matthew Bowers <mlbowers@mit.edu>
2023-07-03 22:17:00 -0400

    added extra options

--- sam_test.py
+++ sam_test.py
@@ -30,6 +30,16 @@ sam = sam_model_registry[model](checkpoint=f"../shared/sam/{path}")
 sam.to(device=0)
 mask_generator = SamAutomaticMaskGenerator(sam)
 
+# SamAutomaticMaskGenerator(
+#     model=sam,
+#     points_per_side=32,
+#     pred_iou_thresh=0.86,
+#     stability_score_thresh=0.92,
+#     crop_n_layers=1,
+#     crop_n_points_downscale_factor=2,
+#     min_mask_region_area=100,  # Requires open-cv to run post-processing
+# )
+
 
 img_path = "atari-benchmarks/frostbite_1/674.png"
 

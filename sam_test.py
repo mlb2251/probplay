756ba5d4f88fe267067f3fa20753dcdac5602a5c
Matthew Bowers <mlbowers@mit.edu>
2023-07-03 22:09:50 -0400

    SAM working on csail

--- sam_test.py
+++ sam_test.py
@@ -10,28 +10,30 @@ def show_anns(anns):
     if len(anns) == 0:
         return
     sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
-    # ax = plt.gca()
-    # ax.set_autoscale_on(False)
 
     img = np.ones((sorted_anns[0]['segmentation'].shape[0], sorted_anns[0]['segmentation'].shape[1], 3))
     for ann in sorted_anns:
         m = ann['segmentation']
         color_mask = np.random.random(3)
         img[m] = color_mask
-    # ax.imshow(img)
 
     return img
 
-# plt.figure(figsize=(5,5))
-# plt.imshow(img)
-# plt.axis('off')
-# plt.show()
+# on brief examination runtimes are similar enough and H model
+# seems higher quality
+SAM_H = ("vit_h","sam_vit_h_4b8939.pth") # 2-2.5s and 6.8GB
+SAM_L = ("vit_l","sam_vit_l_0b3195.pth") # 2-2.5s and 5.3GB
+SAM_B = ("vit_b","sam_vit_b_01ec64.pth") # 1.5s and 4.4GB
 
-
-sam = sam_model_registry["default"](checkpoint="/Users/matthewbowers/proj/shared/sam/sam_vit_h_4b8939.pth")
+(model,path) = SAM_H
+sam = sam_model_registry[model](checkpoint=f"../shared/sam/{path}")
+sam.to(device=0)
 mask_generator = SamAutomaticMaskGenerator(sam)
 
-with Image.open("atari-benchmarks/frostbite_1/674.png") as im:
+
+img_path = "atari-benchmarks/frostbite_1/674.png"
+
+with Image.open(img_path) as im:
     img = np.asarray(im)
 
 
@@ -45,14 +47,8 @@ mask_img = show_anns(masks)
 
 res = np.concatenate([img/255, mask_img], axis=1)
 
+import os
+os.makedirs("out/sam/", exist_ok=True)
+Image.fromarray((res*255).astype("uint8")).save(f"out/sam/{model}.png")
 
-Image.fromarray((res*255).astype("uint8")).save("out.png")
-
-# plt.figure(figsize=(5,5))
-# plt.imshow(res)
-# plt.axis('off')
-# plt.show()
-
-
-# plt.show()
 # breakpoint()
